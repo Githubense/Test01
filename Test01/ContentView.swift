b2c4efdd6c70bee5381f42f665750451d84686b3
Githubense <angelopf@outlook.com>
2024-10-14 16:31:34 +0200

    Initial Commit

---
+++ Test01/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Test01
+//
+//  Created by Angel Adrian Pimienta Flores on 14/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Test01/Test01App.swift
@@ -0,0 +1,17 @@
+//
+//  Test01App.swift
+//  Test01
+//
+//  Created by Angel Adrian Pimienta Flores on 14/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Test01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
